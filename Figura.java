ba4cebf8f1370e1d3284d55016daa67debb718f1
Imanol <itrebar279@g.educaand.es>
2024-02-23 12:38:58 +0100

    Modificar el documneto con comentarios

--- Figura.java
+++ Figura.java
@@ -1,4 +1,13 @@
+
+/*Representa figuras geometricas
+Las su0bclases deben implementar los metodos para calcular area y perimetro
+@author
+
+*/
+
 public abstract class Figura {
+/*Retorna el area de la figura*/
 public abstract double area();
+/*Retorna el perimetro de la figura*/
 public abstract double perimetro();
 }
